b45864b2b637851048d72da9803bb37cf6939186
Harish <a.harish3105@gmail.com>
2022-12-13 06:24:21 +0000

    CodingPractice1 Super Over League

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="background">
+    <h1 className="League">Super Over League</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      className="logo-image"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      className="logo-image"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
